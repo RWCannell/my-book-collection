2a27c22cd00218211ce500981739980cc1def913
RWCannell <regancannell@gmail.com>
2023-06-11 13:56:26 +0200

    Removing Typescript and starting from scratch with Javascript

--- src/db/connection.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from "pg";
-
-export default new Pool ({
-    max: 20,
-    connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@http://localhost:5400/${process.env.POSTGRES_DB}`,
-    idleTimeoutMillis: 30000
-});
--- src/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import BooksController from "./controllers/BooksController";
-
-const router = Router();
-const booksController = new BooksController();
-
-router.get('/', booksController.get);
-
-export default router;
