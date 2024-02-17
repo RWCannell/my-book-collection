8bb2012c9c2c850a2824c0b737855a528aedfeaa
RWCannell <regancannell@gmail.com>
2024-02-17 19:59:55 +0200

    Use js to render cards in container

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-// import Handlebars from "./handlebars";
-import books from "./../data/books.js";
+import data from "./../data/books.js";
 
-// const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Nils" }));
-console.log("books", JSON.stringify(books));
+const cardContent = document.getElementById("cardRow");
+let html = "";
+data.books.forEach(book => {
+    console.log(book.title);
+    html = html + `
+        <div class="col-sm-4">
+            <div class="card">
+                <img src="./images/book_icon.png" class="book-cover" alt="...">
+                <div class="card-body">
+                    <h5 class="book-title"><b>${book.title}</b></h5>
+                    <p class="book-author"><b>Author:</b> ${book.author}</p>
+                    <p class="book-year"><b>Year:</b> ${book.originallyPublished}</p>
+                    <p class="reading-status"><b>Status:</b> ${book.readingStatus}</p>
+                    <a href="${book.readMoreUrl}" target="_blank">Wiki</a>
+                </div>
+            </div>
+        </div>
+    `;
+});
+cardContent.innerHTML = html;
